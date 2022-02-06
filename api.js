652e13abe4e80e3f24ed1ac0d777a13fede939c7
salmasobeh <salma.sobeh97@gmail.com>
2022-02-06 15:30:09 +0200

    getting the name onclick and calling the apis

--- api.js
+++ api.js
@@ -1,3 +1,17 @@
+document.getElementById("btn").addEventListener("click", function() {
+
+    var new_name = document.getElementById("name").value
+    console.log(new_name)
+
+    const url1 = "https://api.genderize.io?name=" + new_name;
+    const url2 = "https://api.agify.io/?name=" + new_name;
+    const url3 = "https://api.nationalize.io/?name=" + new_name;
+
+    getapi1(url1)
+    getapi2(url2)
+    getapi3(url3)
+
+});
 // Defining async functions
 //Gender
 async function getapi1(url) {
